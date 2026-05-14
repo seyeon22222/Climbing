a7dae108b189a421e260aefd586637920ef0730c
seykim <kksy0917@gmail.com>
2026-05-15 01:12:12 +0900

    feature : setup CI and PR template

--- app/src/main/java/com/project/climbing/MainActivity.kt
+++ app/src/main/java/com/project/climbing/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import dagger.hilt.android.AndroidEntryPoint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.navigation.compose.rememberNavController
-import com.project.climbing.navigation.AppNavGraph
-import com.project.climbing.core.ui.theme.ClimbingTheme
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.project.climbing.core.ui.theme.ClimbingTheme
+import com.project.climbing.navigation.AppNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
